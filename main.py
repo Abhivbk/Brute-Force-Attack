0da3f2c85f51e39b0c4ce0fb2c7ba751934b1dfa
Abhinav Kukarni <abhivbk13@gmail.com>
2022-02-06 16:50:40 +0530

    Added PyAutoGui password Dialog

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from msilib.schema import Error
 import random
 import pyautogui
 
 guess = "abcdefghijklmnopqrstuvwxyz1234567890"
 char_list = list(guess)
-password = pyautogui.password('Enter the password').strip().lower()
 guesspswd = ""
 while True:
-    guesspswd = random.choices(char_list, k=len(password))
-    print(guesspswd)
-    if guesspswd == list(password):
-        print("Successfullyy Broke into the access \n Password: {}".format(guesspswd))
-        break
+    try:
+        password = pyautogui.password('Enter the password').strip().lower()
+        guesspswd = random.choices(char_list, k=len(password))
+        print(guesspswd)
+        if guesspswd == list(password):
+            print("Successfullyy Broke into the access \n Password: {}".format(guesspswd))
+            break
+    except Exception as e:
+        print(f"Erorr")
